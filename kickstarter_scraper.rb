5b92351162cc327d2677f04e23c6864c86df6215
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-14 00:36:13 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,7 +10,8 @@ project= kickstarter.css("li.project.grid_4") #.first
 title: project.css("h2.bbcard_name strong a").text
 image link= project.css("div.project-thumbnail a img").attribute("src").value
 description= project.css("p.bbcard_blurb").text
-location= 
+location= project.css(".location-name").text
+
   # write your code here
   binding.pry
 end
